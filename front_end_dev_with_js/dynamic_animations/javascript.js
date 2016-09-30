13b2ccd6185ad8cf9c0858d4a8604bdaaa9461d1
Rachel Minto <rachel.j.minto@gmail.com>
2016-09-30 16:26:53 +0000

    Starting placement added.

--- front_end_dev_with_js/dynamic_animations/javascript.js
+++ front_end_dev_with_js/dynamic_animations/javascript.js
@@ -1,11 +1,36 @@
 $(function() {
+  var $canvas = $('#canvas');
+  
+  function getFormObject($f) {
+    var o = {}
+    
+    $f.serializeArray().forEach(function(input) {
+      o[input.name] = input.value;
+    });
+    
+    return o;
+  } 
+  
+  function createElement(data) {
+    var $d = $("<div />", {
+      "class": data.shape,
+      data: data,
+      css: {
+        left: +data.starting_x,
+        top: +data.starting_y,
+      }
+    });
+    
+    return $d;
+  }
+  
   $('form').submit(function(e) {
     e.preventDefault();
-    var animation_details = $(this).serializeArray();
-    var shape = animation_details[0].value;
-    console.log(shape);
-
-    $('#canvas').append('<div class="' + shape + '", style="top: 100px;">' + '</div>')
-
+    var $f = $(this)
+    var data;
+    
+    data = getFormObject($f);
+    console.log(data);
+    $canvas.append(createElement(data));
   });
 });
