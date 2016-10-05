29e0c9b1e3ebf6bc3c6baa5b1d82e8d87a214555
RachelMinto <rachel.j.minto@gmail.com>
2016-10-05 10:07:46 -0400

    Refactoring to be able to use .next and .prev

--- front_end_dev_with_js/gallery_server/public/javascripts/gallery.js
+++ front_end_dev_with_js/gallery_server/public/javascripts/gallery.js
@@ -1,65 +1,22 @@
-$(document).ready(function() {
-  var currentID = 1;
-  var photoData = []
-  $.ajax({
+$(function() {
+  var slideshow = {
+    speed: 500,
+    photosData: [],
+    currentID: 1,
+    init: function() {
+      this.photosData = getPhotoData;
+      renderPhoto;
+    },
+  };
+  var getPhotoData = $.ajax({
     url: '../photos',
     type: "GET",
     dataType: "json"
   })
   .done(function(photos) {
-    photoData = photos
-    renderPhotoAndData();
+    return photos;
   })
   .fail(function(xhr, status, errorThrown) {
     console.log("sorry, there was a problem!");
-  })
-  renderPhotoAndData = function(){
-    renderPhoto();
-    renderPhotoInfo();
-    renderComments();
-  }
-  renderPhoto = function() {
-    tmpl = $('#photos').html();
-    photo_template = Handlebars.compile(tmpl);
-    $('#slides').empty().append(photo_template({ photos: photoData }));
-  };
-  renderPhotoInfo = function() {
-    photo_template = Handlebars.compile($('#photo_information').html());
-    imagePosition = currentID - 1
-    $('section > header').empty().append(photo_template(photoData[imagePosition]));    
-  };
-  renderComments = function() {
-    $.ajax({
-      url: 'comments?photo_id=' + currentID,
-      type: "GET",
-      dataType: "json"
-    }).done(function(photoComments) {
-      tmpl = $('#commentsFormat').html();
-      comment_template = Handlebars.compile(tmpl);
-      Handlebars.registerPartial('comment', $('#comment').html());
-      $('#comments ul').empty().append(comment_template({ comments: photoComments }));      
-    });
-    $('.prev').off().on("click", function(e) {
-      e.preventDefault();
-      if (+currentID > 1) {
-        currentID -= 1;
-      } else {
-        currentID = 3;
-      };
-      renderPhotoAndData();
-      console.log(currentID);
-    });
-    $('.next').off().on("click", function(e) {
-      e.preventDefault();
-
-      if (+currentID < 3) {
-        currentID += 1;
-      } else {
-        currentID = 1;
-      };
-      renderPhotoAndData();
-      console.log(currentID);
-    });
-  };
+  });
 });
-
