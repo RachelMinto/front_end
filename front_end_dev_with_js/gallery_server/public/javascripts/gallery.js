1f0ba9f97867874b6242878591243d862df5b79f
RachelMinto <rachel.j.minto@gmail.com>
2016-10-06 07:17:30 -0400

    Prev and NExt functions working properly on photos. Need to update comments with slide advance.

--- front_end_dev_with_js/gallery_server/public/javascripts/gallery.js
+++ front_end_dev_with_js/gallery_server/public/javascripts/gallery.js
@@ -1,22 +1,72 @@
 $(function() {
-  var slideshow = {
-    speed: 500,
-    photosData: [],
-    currentID: 1,
-    init: function() {
-      this.photosData = getPhotoData;
-      renderPhoto;
-    },
-  };
-  var getPhotoData = $.ajax({
-    url: '../photos',
-    type: "GET",
-    dataType: "json"
-  })
-  .done(function(photos) {
-    return photos;
-  })
-  .fail(function(xhr, status, errorThrown) {
-    console.log("sorry, there was a problem!");
+  var templates = {};
+  var photos;
+  var speed = 500;
+
+  $("script[type='text/x-handlebars']").each(function() {
+    var $tmpl = $(this);
+    templates[$tmpl.attr("id")] = Handlebars.compile($tmpl.html());
   });
+
+  $("[data-type='partial']").each(function() {
+    var $partial = $(this);
+    Handlebars.registerPartial($partial.attr("id"), $partial.html());
+  });
+
+  $.ajax({
+    url: "/photos",
+    success: function(json) {
+      photos = json;
+      renderPhoto();
+      renderPhotoInformation(0);
+      getCommentsFor(photos[0].id);
+    }
+  });
+
+  function renderPhoto() {
+    $('#slides').html(templates.photos({ photos: photos }));
+  };
+
+  function renderPhotoInformation(idx) {
+    $('section > header').html(templates.photo_information(photos[idx]));
+  };
+
+  function getCommentsFor(idx) {
+    $.ajax({
+      url: "/comments",
+      data: "photo_id=" + idx,
+      success: function(comment_json) {
+        $("#comments ul").html(templates.comments({ comments: comment_json }));
+      }
+    });
+  };
+
+  $('.prev').off().on("click", function(e) {
+    e.preventDefault();
+    previousSlide();
+  });
+
+  $('.next').off().on("click", function(e) {
+    e.preventDefault();
+    nextSlide();
+  }); 
+
+  function nextSlide() {
+    if ($('#slides figure:visible').is(":last-child")) {
+      $('#slides figure:visible').fadeOut(speed).siblings(":first-child").fadeIn(speed);
+    } else {
+      $('#slides figure:visible').fadeOut(speed).next().fadeIn(speed);
+    }
+  };
+
+  function previousSlide() {
+    if ($('#slides figure:visible').is(":first-child")) {
+      $('#slides figure:visible').fadeOut(speed).siblings(":last-child").fadeIn(speed);
+    } else {
+      $('#slides figure:visible').fadeOut(speed).prev().fadeIn(speed);
+    }
+  };
+
 });
+
+
