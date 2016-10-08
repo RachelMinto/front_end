73370f15257096f53848c7ffe189c8327fca5b2e
RachelMinto <rachel.j.minto@gmail.com>
2016-10-08 09:58:38 -0400

    Slideshow application using AJAX requests to update content.

--- front_end_dev_with_js/gallery_server/public/javascripts/gallery.js
+++ front_end_dev_with_js/gallery_server/public/javascripts/gallery.js
@@ -13,12 +13,91 @@ $(function() {
     Handlebars.registerPartial($partial.attr("id"), $partial.html());
   });
 
+  $('section > header').on("click", '.actions a', function(e) {
+    e.preventDefault();
+    var $e = $(e.target);
+
+    $.ajax({
+      url: $e.attr("href"),
+      type: "post",
+      data: "photo_id=" + $e.data('id'),
+      success: function(json) {
+        $e.text(function(i, txt) {
+          return txt.replace(/\d+/, json.total);
+        })
+      }
+    });
+  });
+
+  $('form').on("submit", function(e) {
+    e.preventDefault();
+    var $f = $(this);
+
+    $.ajax({
+      url: $f.attr("action"),
+      type: $f.attr("method"),
+      data: $f.serialize(),
+      success: function(json) {
+        $('#comments ul').append(templates.comment(json));
+      }
+    });
+    $f.trigger('reset');
+  });
+
+  var slideshow = {
+    $el: $("#slideshow"),
+    duration: 500,
+    prevSlide: function(e) {
+      e.preventDefault();
+      var $current = this.$el.find("figure:visible");
+      var $prev = $current.prev("figure");
+
+      if (!$prev.length) {
+        $prev = this.$el.find("figure").last();
+      }
+      $current.fadeOut(this.duration);
+      $prev.fadeIn(this.duration);
+      this.renderPhotoContent(+$prev.data('id'));
+    },
+    nextSlide: function(e) {
+      e.preventDefault();
+      var $current = this.$el.find("figure:visible");
+      var $next = $current.next("figure");
+
+      if (!$next.length) {
+        $next = this.$el.find("figure").first();
+      }
+
+      $current.fadeOut(this.duration);
+      $next.fadeIn(this.duration);
+      this.renderPhotoContent(+$next.data('id'));
+    },
+    incrementLikes: function(e) {
+      e.preventDefault();
+      console.log('i lurv it!');
+    },
+    renderPhotoContent: function(idx) {
+      $("[name=photo_id]").val(idx);
+      renderPhotoInformation(idx);
+      getCommentsFor(idx);
+    },
+    bind: function() {
+      this.$el.find("a.prev").on("click", this.prevSlide.bind(this));
+      this.$el.find("a.next").on("click", this.nextSlide.bind(this));
+    },
+
+    init: function() {
+      this.bind();
+    }
+  };
+
   $.ajax({
     url: "/photos",
     success: function(json) {
       photos = json;
       renderPhoto();
-      renderPhotoInformation(0);
+      renderPhotoInformation(photos[0].id);
+      slideshow.init();
       getCommentsFor(photos[0].id);
     }
   });
@@ -28,7 +107,10 @@ $(function() {
   };
 
   function renderPhotoInformation(idx) {
-    $('section > header').html(templates.photo_information(photos[idx]));
+    var photo = photos.filter(function(item) {
+      return item.id === idx;
+    })[0];
+    $('section > header').html(templates.photo_information(photo));
   };
 
   function getCommentsFor(idx) {
@@ -40,33 +122,4 @@ $(function() {
       }
     });
   };
-
-  $('.prev').off().on("click", function(e) {
-    e.preventDefault();
-    previousSlide();
-  });
-
-  $('.next').off().on("click", function(e) {
-    e.preventDefault();
-    nextSlide();
-  }); 
-
-  function nextSlide() {
-    if ($('#slides figure:visible').is(":last-child")) {
-      $('#slides figure:visible').fadeOut(speed).siblings(":first-child").fadeIn(speed);
-    } else {
-      $('#slides figure:visible').fadeOut(speed).next().fadeIn(speed);
-    }
-  };
-
-  function previousSlide() {
-    if ($('#slides figure:visible').is(":first-child")) {
-      $('#slides figure:visible').fadeOut(speed).siblings(":last-child").fadeIn(speed);
-    } else {
-      $('#slides figure:visible').fadeOut(speed).prev().fadeIn(speed);
-    }
-  };
-
 });
-
-
