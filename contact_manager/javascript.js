51c60801d9cd1e2db31521711fd9522d93e04e5e
Rachel Minto <rachel.j.minto@gmail.com>
2016-10-19 17:00:45 -0400

    Creating templates.

--- contact_manager/javascript.js
+++ contact_manager/javascript.js
@@ -1,17 +1,23 @@
-
-
 var $main = $('main');
+var templates = {};
 
 function contactManager() {
   this.init();
 }
 
 contactManager.prototype = {
-  collection: [],
+  currentContacts: [],
+  currentID: '',
+
   init: function() {
     this.bind();
+    this.cacheTemplates();
+    debugger;
+    this.loadLocalStorage();
+    this.loadMainContent();
   },
   cacheTemplates: function() {
+    debugger;
     $("script[type='text/x-handlebars']").each(function() {
       var $tmpl = $(this).remove();
       templates[$tmpl.attr("id")] = Handlebars.compile($tmpl.html());
@@ -22,10 +28,16 @@ contactManager.prototype = {
       Handlebars.registerPartial($partial.attr("id"), $partial.html());
     });
   },
+  loadLocalStorage: function() {
+    current_id = localStorage.getItem('current_id') || '0';
+    currentContacts = JSON.parse(localStorage.getItem('contacts'));
+  },
+  loadMainContent: function() {
+    $('main').prepend($(templates['main_content']({})));
+  },
   openModal: function() {
     console.log('evne better!');
   },
-
   bind: function() {
     $main.on("click", this.openModal.bind(this));
   },
@@ -33,9 +45,8 @@ contactManager.prototype = {
 
 contacts = new contactManager();
 
-
-// $(window).on('unload', function() {
-//   localStorage.removeItem('todos');
-//   localStorage.setItem('todos', JSON.stringify(toDos.collection));
-//   localStorage.setItem('current_id', this.current_id.toString());
-// });
+$(window).on('unload', function() {
+  localStorage.removeItem('contacts');
+  localStorage.setItem('contacts', JSON.stringify(contacts.currentContacts));
+  localStorage.setItem('currentID', contacts.currentID.toString());
+});
