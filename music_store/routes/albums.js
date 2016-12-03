3c8ceb089560df944e4aff52589ed6907cc14b9b
RachelMinto <rachel.j.minto@gmail.com>
2016-12-03 15:14:26 -0500

    Added put and delete

--- music_store/routes/albums.js
+++ music_store/routes/albums.js
@@ -1,14 +1,27 @@
 var path = require("path");
+var _ = require("underscore");
 Albums = require(path.dirname(__dirname) + "/routes/album_node");
 
 module.exports = function(router) {
-  router.post("/albums", function(req, res) {
+  router.route("/albums").get(function(req, res) {
+    res.json(Albums.get());
+  }).post(function(req, res) {
     var album = req.body;
     Albums.set(album)
     res.json(album) // response. Automatically sets content headers so content is JSON.
-  });
+  }).put(function(req, res) {
+    var albums = Albums.get();
+    var current_album = _(albums).findWhere({ id: +req.body.id });
 
-  router.get("/albums/new", function(req, res) {
-    res.render("new");
+    _.extend(current_album, req.body);
+    Albums.set(albums);
+    res.json(current_album);
+  }).delete(function(req, res) {
+    var albums = _(Albums.get()).reject(function(a) {
+      return a.id === +req.body.id;
+    });
+
+    Albums.set(albums);
+    res.status(200).end();
   });
 };
