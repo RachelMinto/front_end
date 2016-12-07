038caa05102869319f08986249161571b75cd1ea
RachelMinto <rachel.j.minto@gmail.com>
2016-12-06 19:58:55 -0500

    Working hover for index elements.

--- sushi_website/public/javascripts/handlebars_templates.js
+++ sushi_website/public/javascripts/handlebars_templates.js
@@ -5,7 +5,7 @@ this["JST"]["food_item"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main"
 
   return "<figure><img src=\""
     + alias4(((helper = (helper = helpers.image || (depth0 != null ? depth0.image : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"image","hash":{},"data":data}) : helper)))
-    + "\" /></figure><figcaption><div><h2>"
+    + "\"/><div class=\"overlay\"><img src=\"/images/nav-next.png\"/></div></figure><figcaption><div><h2>"
     + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
     + "</h2><p>"
     + alias4(((helper = (helper = helpers.price || (depth0 != null ? depth0.price : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"price","hash":{},"data":data}) : helper)))
