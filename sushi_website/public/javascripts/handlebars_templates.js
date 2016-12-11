6b44eb3026b1583e7065c4dae965a4242257d71e
RachelMinto <rachel.j.minto@gmail.com>
2016-12-11 18:50:10 -0500

    Fixed css for header cart.

--- sushi_website/public/javascripts/handlebars_templates.js
+++ sushi_website/public/javascripts/handlebars_templates.js
@@ -55,11 +55,11 @@ this["JST"]["food_item"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main"
 
   return "<figure><img src=\""
     + alias4(((helper = (helper = helpers.image || (depth0 != null ? depth0.image : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"image","hash":{},"data":data}) : helper)))
-    + "\"/><div class=\"overlay\"><img src=\"/images/nav-next.png\"/></div></figure><figcaption><div><h2>"
+    + "\"/><div class=\"overlay\"><img src=\"/images/nav-next.png\"/></div><figcaption><div><h2>"
     + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
     + "</h2><p>$"
     + alias4((helpers.format_price || (depth0 && depth0.format_price) || alias2).call(alias1,(depth0 != null ? depth0.price : depth0),{"name":"format_price","hash":{},"data":data}))
-    + "</p></div><div><a href=\"\">Add to Cart</a></div></figcaption>";
+    + "</p></div></figcaption></figure><a href=\"\"><div>+ Add to Cart</div></a>";
 },"useData":true});
 
 this["JST"]["header"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
